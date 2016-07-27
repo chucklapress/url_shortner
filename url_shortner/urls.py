29e96f5d95499998c703946924888aa0a8a2205b
chucklapress <charleslapress@gmail.com>
2016-07-27 19:20:17 -0400

    changed bookmark model to foreign key to users and installed hashtags

--- url_shortner/urls.py
+++ url_shortner/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import login, logout
 from urlbook.views import IndexView, NewListView, SignUpView
 
--- urlbook/models.py
+++ urlbook/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,4 +15,4 @@ class BookMark(models.Model):
     description = models.CharField(max_length=80)
     enterdate = models.DateTimeField(auto_now_add=True)
     uniqueid = models.CharField(max_length=20)
-    appuser = models.ForeignKey(Click)
+    appuser = models.ForeignKey(User)
